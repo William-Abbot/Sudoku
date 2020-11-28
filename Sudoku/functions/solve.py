b7d8f60a8613f93f9592a84602fd462326c0cde1
William-Abbot <59292592+William-Abbot@users.noreply.github.com>
2020-11-27 20:33:03 -0600

    Add files via upload

--- Sudoku/functions/solve.py
+++ Sudoku/functions/solve.py
@@ -1,1 +1,11 @@
+#python3!
 #solve.py
+#William Abbot
+
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+#Python, please allow implicit references again!!
+
+from solving import depthFirstSearch
